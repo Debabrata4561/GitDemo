a84a28675cd750c5d3d1096e7f48c3aac909c334
danny <debabrata90@gmail.com>
2023-08-15 23:07:14 +0530

    asian time zone guy

--- src/seleniumrevision/Loginpage.java
+++ src/seleniumrevision/Loginpage.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Loginpage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.setProperty("WebDriver.Chrome.driver", "C:\\Java\\chromedriver.exe.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.flifkart.com");
+	@Test
+	public void jiratest() {
+		System.out.println("jira test");
+		System.out.println("jira test");
+		System.out.println("jira test");
 	}
 
+	@Test
+	public void login() 
+	{
+System.out.println("login to facebook");
+	}
 }
