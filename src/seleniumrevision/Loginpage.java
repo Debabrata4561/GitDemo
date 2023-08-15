19eafa65dc009e481d5dafdfc2e78f46931172fe
danny <debabrata90@gmail.com>
2023-08-15 23:17:28 +0530

    asian time zone guy1

--- src/seleniumrevision/Loginpage.java
+++ src/seleniumrevision/Loginpage.java
@@ -8,13 +8,13 @@ public class Loginpage {
 	@Test
 	public void jiratest() {
 		System.out.println("jira test");
-		System.out.println("jira test");
-		System.out.println("jira test");
+		System.out.println("jira test1");
+		System.out.println("jira test2");
 	}
 
 	@Test
-	public void login() 
-	{
-System.out.println("login to facebook");
+	public void login() {
+		System.out.println("login to facebook");
+		System.out.println("login to facebook1");
 	}
 }
